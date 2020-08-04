ecc01f699b632ccec000df1386b9c3b149759918
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2020-08-04 14:58:16 -0700

    updated reg.py

--- regression/reg.py
+++ regression/reg.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import pandas as pd
+import statistics
 
 x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
 y = np.array([5, 20, 14, 32, 22, 38])
@@ -31,8 +32,8 @@ print(data.head())
 
 # define independent variable - temp C
 temp = data['Temp (C)'].values
-#print(temp)
-
+print("ST DEV:", statistics.stdev(temp))
+print("Mean:", statistics.mean(temp))
 # define dependent variable - humidity
 relHum = data['Rel Hum (%)'].values
 #print(relHum)
